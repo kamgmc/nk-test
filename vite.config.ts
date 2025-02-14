2db86f6da8e8d09e36d03e44b8d0fedca18c4f11
Kevin Martinez <kamgmc@gmail.com>
2025-02-14 17:28:29 -0300

    Fix tailwind render

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +8,5 @@ export default defineConfig({
       "@": "/src",
     },
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
